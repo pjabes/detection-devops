df82b834f10b40d6e5118e884f2bd912660570a1
Patrick Abraham <patrick@pabes.me>
2021-05-27 23:17:24 +1000

    Updated tempalte for detection library

--- scripts/build_library.py
+++ scripts/build_library.py
@@ -3,21 +3,43 @@ import yaml
 
 template = """
 
-## Detection
-Name: {{ friendly_name }}
-Description: {{ description }}
-Detection Type: {{ type }}
-Author: {{ author }}
-Created: {{ created }}
-Last Updated: {{ last_updated }}
+## {{detection.friendly_name}}
+
+**Description**: {{ detection.description }}  
+**Detection Type**: {{ detection.type }}
+
 ---
 
-## Dependencies
+### Detection Concepts
+**Intention**: {{ detection.intention }}  
+**Known FPs**:  
+{% for known_fp in detection.known_falsepositives %}
+    * {{ known_fp }}
+{% endfor %}  
+**Blindspots**: 
+{% for blindspot in detection.blindspots %}
+    * {{ blindspot }}
+{% endfor %}
+**Detection Dependencies**:  
+Logging Policies (LPs):  
+{% for LP in detection.dependencies.LPs %}
+    * {{ LP }}
+{% endfor %}
+Hardening Policies (HPs):  
+{% for HP in detection.dependencies.HPs %}
+    * {{ HP }}
+{% endfor %}
+
+---
+## Response 
+
 
 """
+
 with open("../detections/DetectDNSRequestsToPhishingSites.yaml") as file:
     detectionFile = yaml.load(file, Loader=yaml.FullLoader)
 
+print(detectionFile)
 j2_template = Template(template)
 
 output = j2_template.render(detectionFile)
