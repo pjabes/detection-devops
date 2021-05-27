50146e41bc93177606a568ba132cbafe7c9d70d9
Patrick Abraham <patrick@pabes.me>
2021-05-27 22:20:07 +1000

    Enhancing detection schema for library creation

---
+++ scripts/build_library.py
@@ -0,0 +1,28 @@
+from jinja2 import Template
+import yaml
+
+template = """
+
+## Detection
+Name: {{ friendly_name }}
+Description: {{ description }}
+Detection Type: {{ type }}
+Author: {{ author }}
+Created: {{ created }}
+Last Updated: {{ last_updated }}
+
+---
+
+## Dependencies
+
+
+"""
+with open("../detections/DetectDNSRequestsToPhishingSites.yaml") as file:
+    detectionFile = yaml.load(file, Loader=yaml.FullLoader)
+
+j2_template = Template(template)
+
+output = j2_template.render(detectionFile)
+print(output)
+with open('../docs/DetectDNSRequestsToPhishingSites.md', 'w') as file:
+    file.write(output)
